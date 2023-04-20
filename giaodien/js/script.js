d6c50098b25f61063362dc9a18178cd90e37fab9
Antong <togan3006@gmail.com>
2023-04-21 00:29:03 +0700

    giaodien_part1

---
+++ giaodien/js/script.js
@@ -0,0 +1,41 @@
+const  itemsliderbar = document.querySelectorAll(".cartegory-left-li")
+itemsliderbar.forEach(function(menu,index){
+    menu.addEventListener("click",function(){
+        menu.classList.toggle("block")
+    })
+})
+
+// Product
+
+const baoquan = document.querySelector(".baoquan")
+const chitiet = document.querySelector(".chitiet")
+if(baoquan){
+    baoquan.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block"
+    })
+}
+if(chitiet){
+    chitiet.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+    })
+}
+
+// product-img
+
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+smallImg.forEach(function(imgItem,x){
+    imgItem.addEventListener("click",function(){
+        bigImg.src = imgItem.src
+    })
+})
+
+
+const butIon = document.querySelector(".product-content-right-bottom-top")
+if(butIon){
+    butIon.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
+    })
+}
